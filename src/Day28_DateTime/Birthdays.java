5ed004aee874edceb1f476da5ee9082241db3b0b
Rahman Abdalla <rahman.abdalla43@gmail.com>
2020-04-24 14:28:31 -0400

    Birthdays.java

--- src/Day28_DateTime/Birthdays.java
+++ src/Day28_DateTime/Birthdays.java
@@ -6,17 +6,17 @@ import java.time.LocalDateTime;
 public class Birthdays {
     public static void main(String[] args) {
 
-        HappyBirthDay(1990, 4, 25);
+        HappyBirthDay(1990, 4, 25,"Rahman");
 
     }
 
-    public static void HappyBirthDay(int year, int month, int day) {
+    public static void HappyBirthDay(int year, int month, int day, String name) {
         LocalDate birthday = LocalDate.of(year, month, day);
         LocalDate today = LocalDate.now();
         int month2 = today.getMonthValue();
         int day2 = today.getDayOfMonth();
         if (month2 == month && day == day2) {
-            System.out.println("Today is your birthday!!!");
+            System.out.println("Today is your birthday!!!"+name);
             System.out.println("   \n \t \t  \t \t    * * * * * * * * * * * * * * * * * * * * *");
             System.out.println("   \t \t  \t \t  *  * * * * * * * * * * * * * * * * * * * *  * ");
             System.out.println("  \t \t  \t \t *  *                                       *  * ");
@@ -26,7 +26,7 @@ public class Birthdays {
             System.out.println("   \t \t   \t \t    * * * * * * * * * * * * * * * * * * * * *");
             return;
         }
-        System.out.println("Today is not your birthday yet");
+        System.out.println("Today is not your birthday yet "+name);
     }
 
 
