d0a36bba337fd0e1fca32d4b683857fd4a06f3e9
Rahman Abdalla <rahman.abdalla43@gmail.com>
2020-04-27 10:27:07 -0400

    WrapperClass

--- src/Day29_Wrapper_ArrayList/WrapperClass.java
+++ src/Day29_Wrapper_ArrayList/WrapperClass.java
@@ -3,7 +3,24 @@ package Day29_Wrapper_ArrayList;
 public class WrapperClass {
     public static void main(String[] args) {
 
-        int n = 100;
+        int a = 100;
 
+        Integer b = a; // Wrapper class, it only is Loyal to int
+
+        short s1 = 200;
+        int n1 = s1;
+
+       // Integer N1 = s1; // Error coz Integer is dedicated to int primitive ONLY
+        Integer n2 = n1;
+
+        int a1 = 20;
+        float f1 = a1;
+        // Float f2 = a1;
+
+        Integer I1 = 200;
+        int a2 = I1;
+
+        Byte B1 = 126; //But there's NO restriction when assigning a wrapper class to the primitives
+        int a3 = B1;
     }
 }
